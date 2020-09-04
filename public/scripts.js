8b0d029161e9f0fa58bfe7605765cb5967cca73f
iamrifki <iamrifki0@gmail.com>
2020-09-04 09:52:02 +0700

    Remove leftover code I was trying to implement a comments system, but it ended with failiure. Plus, I don't think anyone would really use it aside from spammers.

--- public/scripts.js
+++ public/scripts.js
@@ -33,17 +33,6 @@ function increaseThanksLocally() {
   localStorage.setItem("localCount", count);
 }
 
-/* Text Counter */
-
-function textCounter(field, cnt, maxlimit) {
-  var cntfield = document.getElementById(cnt);
-  if (field.value.length > maxlimit)
-    // if too long...trim it!
-    field.value = field.value.substring(0, maxlimit);
-  // otherwise, update 'characters left' counter
-  else cntfield.value = maxlimit - field.value.length;
-}
-
 /* Play Music */
 
 let isAudioEnabled = true;
